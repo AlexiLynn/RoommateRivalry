75eae590dffb77662c911f779575563cf5ca9a58
Alexi <alringle@calpoly.edu>
2024-02-22 16:05:15 -0800

    Frontent Refactor

--- packages/frontend/src/components/Table.js
+++ packages/frontend/src/components/Table.js
@@ -20,24 +20,16 @@ const HardcodedTable = () => {
         <tr>
           <td>2</td>
           <td>
-            <a href="/roommates" style={{ color: 'black', textAlign: 'center', textDecoration: 'none', borderBottom: '1px solid transparent' }}
-              onMouseOver={(e) => (e.currentTarget.style.borderBottom = '1px solid black')}
-              onMouseOut={(e) => (e.currentTarget.style.borderBottom = '1px solid transparent')}>
               Johnny
-            </a>
           </td>
           <td>3pts</td>
         </tr>
         <tr>
           <td>3</td>
           <td>
-            <a href="/roommates" style={{ color: 'black', textAlign: 'center', textDecoration: 'none', borderBottom: '1px solid transparent' }}
-              onMouseOver={(e) => (e.currentTarget.style.borderBottom = '1px solid black')}
-              onMouseOut={(e) => (e.currentTarget.style.borderBottom = '1px solid transparent')}>
-            Amy
-            </a>
+              Amy
           </td>
-          <td style={{ color: 'red' }}>0pts</td>
+          <td style={{ color: "red" }}>0pts</td>
         </tr>
       </tbody>
     </table>
